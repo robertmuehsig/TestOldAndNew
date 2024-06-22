3e8a4b7857bed4a0da4bc8d35ae649e03f4f1564
Robert Muehsig <robert.muehsig@codeinside.eu>
2024-06-22 23:51:22 +0200

    test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,9 @@
-using CommonTypes;
+﻿using CommonTypes;
+using static System.Formats.Asn1.AsnWriter;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.Metadata;
+using System.Text;
 
 namespace TestProject1
 {
@@ -12,6 +17,18 @@ namespace TestProject1
             Person person = new Person() { Name = "Test" };
 
             // throws
+            // Could not load type 'System.Runtime.CompilerServices.RequiredMemberAttribute' from assembly 'BclTest.BclTypes, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null'.
+            //
+            //  Stack Trace: 
+            //    ModuleHandle.ResolveType(QCallModule module, Int32 typeToken, IntPtr * typeInstArgs, Int32 typeInstCount, IntPtr * methodInstArgs, Int32 methodInstCount, ObjectHandleOnStack type)
+            //    ModuleHandle.ResolveTypeHandle(Int32 typeToken, RuntimeTypeHandle[] typeInstantiationContext, RuntimeTypeHandle[] methodInstantiationContext)
+            //    RuntimeModule.ResolveType(Int32 metadataToken, Type[] genericTypeArguments, Type[] genericMethodArguments)
+            //    CustomAttribute.FilterCustomAttributeRecord(MetadataToken caCtorToken, MetadataImport & scope, RuntimeModule decoratedModule, MetadataToken decoratedToken, RuntimeType attributeFilterType, Boolean mustBeInheritable, ListBuilder`1 & derivedAttributes, RuntimeType & attributeType, IRuntimeMethodInfo & ctorWithParameters, Boolean & isVarArg)
+            //    CustomAttribute.IsCustomAttributeDefined(RuntimeModule decoratedModule, Int32 decoratedMetadataToken, RuntimeType attributeFilterType, Int32 attributeCtorToken, Boolean mustBeInheritable)
+            //    CustomAttribute.IsDefined(RuntimeType type, RuntimeType caType, Boolean inherit)
+            //    StackTrace.ToString(TraceFormat traceFormat, StringBuilder sb)
+            //    StackTrace.ToString(TraceFormat traceFormat)
+            //    Exception.get_StackTrace()
             string xml = @"<Person><Name>John Doe</Name></Person>";
             Person personX = Person.DeserializeFromXml(xml);
             Console.WriteLine(personX.Name);
