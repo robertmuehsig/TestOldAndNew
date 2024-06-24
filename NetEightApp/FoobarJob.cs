47fb6c66b265398969c444bf767484aa48b42ec4
Robert Muehsig <robert.muehsig@codeinside.eu>
2024-06-24 08:02:12 +0200

    test

---
+++ NetEightApp/FoobarJob.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetEightApp
+{
+    internal class FoobarJob
+    {
+        public void Run([NotNullWhen(returnValue: false)] out string? errorMessage)
+        {
+            errorMessage = null;
+            Console.WriteLine("Running FoobarJob");
+        }
+    }
+}
